545a24b1ce9db41eb50c65e3934fa267f51f55ae
Gerardo2025 <gerardonuevo1234321@gmail.com>
2025-11-17 15:23:53 -0700

    se elimina socket

--- src/pages/utils/socket.js
+++ src/pages/utils/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+//const socket = io("http://localhost:3001");
 
 // Evento que se ejecuta al recibir respuesta del servidor
-socket.on("respuesta", (data) => {
+/*socket.on("respuesta", (data) => {
   console.log("Respuesta del servidor:", data);
 });
 
@@ -13,5 +13,5 @@ export const enviarMensaje = (texto) => {
 };
 
 // Exportar el socket si quieres usar eventos directamente
-export default socket;
+export default socket;*/
 
